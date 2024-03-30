c4de7fdcdbafe96fc2ea4be891c3c7661edbe37f
ebochkov <ebochkov@itkey.com>
2024-03-30 12:37:22 +0300

    wip kubernetes cluster

--- tf_2/variables.tf
+++ tf_2/variables.tf
@@ -10,9 +10,9 @@ variable "folder_id" {
   type = string
 }
 
-variable "service_account_id" {
-  type = string
-}
+#variable "service_account_id" {
+#  type = string
+#}
 
 variable "service_account_name" {
   type = string
