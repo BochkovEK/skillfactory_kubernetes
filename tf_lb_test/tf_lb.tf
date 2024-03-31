724797d40a246587bb5ea5f9ca951080f18b8c88
ebochkov <ebochkov@itkey.com>
2024-03-31 09:58:06 +0300

    wip kubernetes cluster

--- tf_lb_test/tf_lb.tf
+++ tf_lb_test/tf_lb.tf
@@ -33,38 +33,112 @@ resource "yandex_vpc_subnet" "subnet-a" {
   v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
 }
 
+#resource "yandex_vpc_security_group" "k8s-main-sg" {
+#  description = "Security group ensure the basic performance of the cluster. Apply it to the cluster and node groups."
+#  name        = local.main_security_group_name
+#  network_id  = yandex_vpc_network.k8s-network.id
+#
+#  ingress {
+#    protocol       = "ANY"
+#    description    = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера и сервисов."
+#    v4_cidr_blocks = ["10.96.0.0/16", "10.112.0.0/16"]
+#    from_port      = 0
+#    to_port        = 65535
+#  }
+#
+#  ingress {
+#    description    = "The rule allows connection to Git repository by SSH on 22 port from the Internet"
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 22
+#  }
+#
+#  ingress {
+#    description    = "The rule allows connection to Git repository by SSH on 2222 port from the Internet"
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 2222
+#  }
+#
+#  ingress {
+#    description    = "The rule allows HTTP connections from the Internet"
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 80
+#  }
+#
+#  ingress {
+#    description       = "The rule allows availability checks from the load balancer's range of addresses. It is required for the operation of a fault-tolerant cluster and load balancer services."
+#    protocol          = "TCP"
+#    predefined_target = "loadbalancer_healthchecks" # The load balancer's address range.
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#
+#  ingress {
+#    description       = "The rule allows the master-node and node-node interaction within the security group"
+#    protocol          = "ANY"
+#    predefined_target = "self_security_group"
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#
+#  ingress {
+#    description    = "The rule allows the pod-pod and service-service interaction. Specify the subnets of your cluster and services."
+#    protocol       = "ANY"
+#    v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
+#    from_port      = 0
+#    to_port        = 65535
+#  }
+#
+#  ingress {
+#    description    = "The rule allows receipt of debugging ICMP packets from internal subnets"
+#    protocol       = "ICMP"
+#    v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
+#  }
+#
+#  ingress {
+#    description    = "The rule allows connection to Kubernetes API on 6443 port from specified network"
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 6443
+#  }
+#
+#  ingress {
+#    description    = "The rule allows connection to Kubernetes API on 443 port from specified network"
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 443
+#  }
+#
+#  egress {
+#    description    = "The rule allows all outgoing traffic. Nodes can connect to Yandex Container Registry, Object Storage, Docker Hub, and more."
+#    protocol       = "ANY"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    from_port      = 0
+#    to_port        = 65535
+#  }
+#
+#   ingress {
+#    description    = "The rule allows incoming traffic from the internet to django app."
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    from_port      = 3000
+#    to_port        = 3500
+#  }
+#}
+#
+
 resource "yandex_vpc_security_group" "k8s-main-sg" {
   description = "Security group ensure the basic performance of the cluster. Apply it to the cluster and node groups."
   name        = local.main_security_group_name
   network_id  = yandex_vpc_network.k8s-network.id
 
-  ingress {
-    protocol       = "ANY"
-    description    = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера и сервисов."
-    v4_cidr_blocks = ["10.96.0.0/16", "10.112.0.0/16"]
-    from_port      = 0
-    to_port        = 65535
-  }
-
-  ingress {
+    ingress {
     description    = "The rule allows connection to Git repository by SSH on 22 port from the Internet"
     protocol       = "TCP"
     v4_cidr_blocks = ["0.0.0.0/0"]
     port           = 22
-  }
-
-  ingress {
-    description    = "The rule allows connection to Git repository by SSH on 2222 port from the Internet"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 2222
-  }
-
-  ingress {
-    description    = "The rule allows HTTP connections from the Internet"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 80
   }
 
   ingress {
@@ -118,15 +192,37 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
     from_port      = 0
     to_port        = 65535
   }
-
-   ingress {
-    description    = "The rule allows incoming traffic from the internet to django app."
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 3000
-    to_port        = 3500
-  }
 }
+
+#resource "yandex_vpc_security_group" "k8s-public-services" {
+#  description = "Security group allows connections to services from the internet. Apply the rules only for node groups."
+#  name        = local.public_services_sg_name
+#  network_id  = yandex_vpc_network.k8s-network.id
+#
+#  ingress {
+#    description    = "The rule allows incoming traffic from the internet to the NodePort port range. Add ports or change existing ones to the required ports."
+#    protocol       = "TCP"
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    from_port      = 3000
+#    to_port        = 3500
+#  }
+#
+##    ingress {
+##    description    = ""
+##    protocol       = "TCP"
+##    v4_cidr_blocks = ["0.0.0.0/0"]
+##    from_port      = 8000
+##    to_port        = 9000
+##  }
+#
+##  ingress {
+##    description    = ""
+##    protocol       = "TCP"
+##    v4_cidr_blocks = ["0.0.0.0/0"]
+##    from_port      = 70
+##    to_port        = 90
+##  }
+#}
 
 resource "yandex_vpc_security_group" "k8s-public-services" {
   description = "Security group allows connections to services from the internet. Apply the rules only for node groups."
@@ -137,24 +233,8 @@ resource "yandex_vpc_security_group" "k8s-public-services" {
     description    = "The rule allows incoming traffic from the internet to the NodePort port range. Add ports or change existing ones to the required ports."
     protocol       = "TCP"
     v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 3000
-    to_port        = 3500
-  }
-
-    ingress {
-    description    = ""
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 8000
-    to_port        = 9000
-  }
-
-  ingress {
-    description    = ""
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 70
-    to_port        = 90
+    from_port      = 30000
+    to_port        = 32767
   }
 }
 
